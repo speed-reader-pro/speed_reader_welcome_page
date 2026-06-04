2fad4679aed37aa584e5502e6796563fd7b08ce6
Gleb Shalimov <sbgleb10@gmail.com>
2026-06-04 21:38:46 +0300

    add link to the app

--- app-20260422.js
+++ app-20260422.js
@@ -39,6 +39,12 @@ const welcomeTemplate = `
                 <path d="M5 12h14M12 5l7 7-7 7"/>
             </svg>
         </a>
+        <a href="https://speed-reader.pro/" class="btn btn-secondary" target="_blank" rel="noreferrer noopener">
+            Try for Mac
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                <path d="M15 3h6v6"/><path d="M10 14 21 3"/><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"/>
+            </svg>
+        </a>
     </div>
 
     <section class="other-extensions">
